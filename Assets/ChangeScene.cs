7871021dd8c80a2a1e9aac363b47f73156318161
Paritosh <contact@iamparitoshsharma.me>
2019-04-04 18:31:41 +0530

    Base implementation

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class ChangeScene : MonoBehaviour {
+
+
+    public int SceneNum = 1;
+	// Use this for initialization
+	void Start () {
+       XRSettings.enabled = false;
+	}
+	
+    public void ChangeSceneTo()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManagerScript : MonoBehaviour {
+
+    public GameObject image;
+
+    public TextMeshProUGUI text;
+    public float scrollSpeed = 10.0f;
+
+    public Text liveText;
+
+    private RectTransform text1RectTransform, text2RectTransform;
+
+    float startPosition;
+
+    TextMeshProUGUI text1, text2, text3;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        text1 = Instantiate(text) as TextMeshProUGUI;
+        text1RectTransform = text1.GetComponent<RectTransform>();
+        text1RectTransform.SetParent(image.transform);
+        text1RectTransform.localPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        text1.text = liveText.text;
+        text1RectTransform.localPosition = new Vector3(text1RectTransform.localPosition.x - scrollSpeed, text1RectTransform.localPosition.y, text1RectTransform.localPosition.z);
+        if (text1RectTransform.localPosition.x < -1200)
+        {
+            text2 = text1;
+            text1 = Instantiate(text2, Vector3.left * 1200,Quaternion.identity,text2.transform) as TextMeshProUGUI;
+        }
+        if (text1RectTransform.localPosition.x + 1200 < -text.preferredWidth)
+        {
+
+            Destroy(text2);
+        }
+    }
+}
