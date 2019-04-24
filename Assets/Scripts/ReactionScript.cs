2b95691a23a9b5076a6e0262691ca6d6551b814f
Paritosh Sharma <paritosh.sharmas@gmail.com>
2019-04-24 17:24:01 +0530

    Added retrieve for live reactions

--- Assets/Scripts/ReactionScript.cs
+++ Assets/Scripts/ReactionScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -10,16 +11,47 @@ using UnityEngine.EventSystems;
 
 public class ReactionScript : MonoBehaviour
 {
-    public string playerName;
+    public Text[] textArray;
     
     DatabaseReference reactions_reference;
+
     
     void Start()
     {
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://hero-firebase-access.firebaseio.com/");
         reactions_reference = FirebaseDatabase.DefaultInstance.GetReference("live reactions");
     }
+
+    private void Update()
+    {
+        reactions_reference.ValueChanged +=  HandleValueChanged;
+        foreach (var text in textArray)
+        {
+            text.fontSize += 1;
+            text.fontSize -= 1;
+        }
+    }
     
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        reactions_reference.OrderByValue().EqualTo(1).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+                Debug.Log("Offline");
+            else
+            {
+                DataSnapshot snapshot = task.Result;
+                int i = 0;
+                foreach (var childSnapshot in snapshot.Children)
+                {
+                    var childValue = childSnapshot.Value.ToString ();
+                    textArray[i].text = childValue;
+                    i++;
+                }
+            }
+        });
+    }
+
     public void incrementReaction()
     {
         if (EventSystem.current.currentSelectedGameObject.name.Equals("Button 1"))
@@ -41,13 +73,13 @@ public class ReactionScript : MonoBehaviour
         switch (choice)
         {
             case 1:
-                reactions_reference.Child("reaction1/player").SetValueAsync(1);
+                reactions_reference.Child("reaction1").SetValueAsync(1);
                 break;
             case 2:
-                reactions_reference.Child("reaction2/player").SetValueAsync(1);
+                reactions_reference.Child("reaction2").SetValueAsync(1);
                 break;
             case 3:
-                reactions_reference.Child("reaction3/player").SetValueAsync(1);
+                reactions_reference.Child("reaction3").SetValueAsync(1);
                 break;
         }
     }
